348d817c10972d4ba6f17feeb350ba07fe8a1d3a
chathawa <chathawa@vols.utk.edu>
2020-08-23 18:26:11 -0400

    Restructured bibtex.js and added id-ed elements around Educational Papers, Technical Reports, and Thesis sections

--- assets/js/bibtex.js
+++ assets/js/bibtex.js
@@ -1,16 +1,34 @@
-const citations = []
+const sections = [
+    {id: "journals", filename: "journal_papers.bib"},
+    {id: "bookchapters", filename: null},
+    {id: "conferences", filename: "conferences_proceedings.bib"},
+    {id: "educational-papers",  filename: null},
+    {id: "posters", filename: "posters.bib"},
+    {id: "technical-reports", filename: null},
+    {id: "thesis", filename: null}
+];
 
-function load_bibtex(filename) {
-    var xhr = new XMLHttpRequest();
-    url = "https://globalcomputing.group/assets/" + filename;
+function load_citations(filename) {
+    let xhr = new XMLHttpRequest();
+    let url = "https://globalcomputing.group/assets/" + filename;
     xhr.open("GET", url);
     xhr.responseType = "text";
     xhr.send();
     console.debug("Fetching external content '" + xhr.url + '; alt. text will be presented if resource is unreachable');
-    return (xhr.status == 200 ? xhr.response : "[citation not available]");
+    if (xhr.status !== 200)
+        return null;
+
+    let result = [];
+    let regexp = /(@inproceedings|@article)/gi;
+    let match = regexp.exec(xhr.response);
+    while (match != null) {
+        result.push(xhr.response.substring(match.index, regexp.lastIndex));
+        match = regexp.exec(xhr.response);
+    }
+    return result;
 }
 
-function create_container(filename, bibtex) {
+function create_container(bibtex) {
     code = document.createElement("code");
     code.class = "tex bibtex";
     code.innerText = bibtex;
@@ -18,28 +36,30 @@ function create_container(filename, bibtex) {
     pre.appendChild(code);
     result = document.createElement("div");
     result.class = "papercite_bibtex";
-    result.id = "papercite_" + filename + "_block";
     result.appendChild(pre);
     return result;
-}
-
-function embed_bibtex(filename, parent) {
-    bibtex = load_bibtex(filename);
-    el = create_container(filename, bibtex);
-    parent.appendChild(el);
 }
 
 document.addEventListener("load", function() {
     timeline = document.getElementsByClassName("scrollable-timeline")[0];
     console.debug("bibtex content loader assumes there is only 1 element with class 'scrollable-timeline'");
 
-    var i = 0;
-    timeline.getElementsByClassName("tl-section with-thumb").forEach(function(section) {
-        section.getElementsByClassName("tl-item pub-item with-thumb").forEach(function(item) {
-            wrapper = item.getElementsByClassName("content-wrapper")[0];
-            console.debug("bibtex content embedder assumes there is only 1 child element with class 'content-wrapper' in each item element");
-            if (filename = citations[i++])
-                embed_bibtex(filename, wrapper);
-        });
-    });
+    let section_i = -1, section = null, citations = [], citation_i;
+    let div_children = timeline.getElementsByTagName("div");
+    let year_sections = timeline.getElementsByClassName("tl-section with-thumb");
+    for (let div_i = 0; div_i < div_children.length; div_i++) {
+        if (div_children[div_i].id === sections[section_i + 1].id) {
+            section_i++;
+            section = sections[section_i];
+            citations = (section.filename == null ? null : load_citations(section.filename));
+            citation_i = 0;
+        }
+        else if (citations != null && year_sections.contains(div_children[div_i])) {
+            div_children[div_i].getElementsByClassName("tl-item pub-item with-thumb").forEach(function(item) {
+               let wrapper = item.getElementsByClassName("content-wrapper")[0];
+               let container = create_container(citations[citation_i++]);
+               wrapper.append(container);
+            });
+        }
+    }
 });
